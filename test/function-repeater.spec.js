329c0eebe453c480ee235c5e6d24e5f783e20d16
Andrew Strader <astrader@resourceammirati.com>
2017-06-21 10:38:17 -0400

    create function repeater

--- test/function-repeater.spec.js
+++ test/function-repeater.spec.js
@@ -1,4 +1,6 @@
 const should = require('should');
+const sinon = require('sinon');
+require('should-sinon');
 const FunctionRepeater = require('../lib/function-repeater');
 
 describe('FunctionRepeater', () => {
@@ -6,5 +8,68 @@ describe('FunctionRepeater', () => {
     it('should exist', () => {
         should(FunctionRepeater).be.ok();
     });
+
+    describe('instance', () => {
+        var targetSpy, target, instance;
+        beforeEach(() => {
+            targetSpy = sinon.spy();
+            target = function () {
+                targetSpy();
+            };
+            instance = new FunctionRepeater(target);
+        });
+        it('should exist', () => {
+            should(instance).be.ok();
+        });
+        it('should not yet call spy', () => {
+            should(targetSpy).not.be.called();
+        });
+        describe('when proxy is called', () => {
+            beforeEach(() => {
+                instance.proxy();
+            });
+            it('should call spy', () => {
+                should(targetSpy).be.called();
+            });
+        });
+
+        describe('beginning to transmit', () => {
+            var receiverSpy, receiver;
+            beforeEach(() => {
+                receiverSpy = sinon.spy();
+                receiver = function () {
+                    receiverSpy();
+                };
+                instance.transmitter.transmit(receiver);
+            });
+            it('should not yet call spy', () => {
+                should(receiverSpy).not.be.called();
+            });
+            describe('when proxy is called', () => {
+                beforeEach(() => {
+                    instance.proxy();
+                });
+                it('should call spy', () => {
+                    should(receiverSpy).be.called();
+                });
+            });
+        });
+    });
+
+    // TODO test that target is called before receivers
+
+    // TODO test transmission.close()
+
+    // TODO test when target throws error
+
+    // TODO test when receiver throws error
+
+    // TODO test context of receiver call
+
+    // TODO test context of target call
+
+    // TODO test target call return value
+
+    // TODO test target call arguments
 
 });
